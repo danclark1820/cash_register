eb351162fdf77e90c0e80c7c0823e2250e64d734
danclark1820 <danclark1820@gmail.com>
2014-02-24 16:01:19 -0500

    added regex user validation

--- cash_register3.rb
+++ cash_register3.rb
@@ -1,9 +1,6 @@
 require 'csv'
 require 'pry'
 
-
-
-
 def currency_conv(float)
   "$%0.2f" % float
 end
@@ -24,7 +21,7 @@ def file_to_hash(filename)
   hash
 end
 
-def create_report(filename, hash, transactions)
+def create_report_file(filename, hash, transactions)
   #This writes quantity & sku to csv output
   #We will add a date field to csv output when we finish eating
   CSV.open(filename, "ab") do |row|
@@ -39,7 +36,27 @@ def create_report(filename, hash, transactions)
       ]
    end
   end
+end
 
+def manager_report(filename, user_date)
+  report_hash = {}
+  CSV.foreach('report.csv', 'r') do |row|
+    if row.include?(user_date)
+      if report_hash.include?(row[0])
+        report_hash[row[0]][:quantity] += row[2].to_i
+        report_hash[row[0]][:revenue] += row[3].to_i
+        report_hash[row[0]][:profit] += row[4].to_i
+      else
+        report_hash[row[0]] = {
+          name: row[1],
+          quantity: row[2].to_i,
+          revenue: row[3].to_f,
+          profit: row[4].to_f
+        }
+      end
+    end
+  end
+  report_hash
 end
 
 products = file_to_hash('products.csv')
@@ -79,70 +96,50 @@ while selection < counter
   puts "Subtotal: #{currency_conv(subtotal)}"
 end
 
-if selection == counter
-  #call complete sale method
-elsif selection == counter + 1
-  #call reporting method
-end
-
-
 #report generation was here
-create_report('report.csv', products, transactions)
+create_report_file('report.csv', products, transactions)
 
-
-#Creates Sale Complete
-puts "===Sale Complete==="
-puts ""
-total = 0.0
-transactions.each do |key, value|
-  puts "$#{products[key][:retail_price] * transactions[key]} - #{transactions[key]} #{products[key][:name]} "
-  total += products[key][:retail_price] * transactions[key]
-end
-puts "\nTotal: #{currency_conv(total)}"
-
-#Get Tendered
-puts "\nWhat is the amount tendered?"
-tendered = gets.chomp.to_f
-
-#Creates Receipt
-puts "===Thank You!==="
-puts "The total change due is #{currency_conv(tendered-total)}\n\n "
-puts Time.now.strftime("%m/%d/%Y %I:%M%p")
-puts "================"
-
-#Creates Report
-puts "What date would you like reports for? (MM/DD/YYYY)"
-user_date = gets.chomp
-puts "On #{user_date} we sold:"
-
-report_hash = {}
-CSV.foreach('report.csv', 'r') do |row|
-  if row.include?(user_date)
-    if report_hash.include?(row[0])
-      report_hash[row[0]][:quantity] += row[2].to_i
-      report_hash[row[0]][:revenue] += row[3].to_i
-      report_hash[row[0]][:profit] += row[4].to_i
-    else
-      report_hash[row[0]] = {
-        name: row[1],
-        quantity: row[2].to_i,
-        revenue: row[3].to_f,
-        profit: row[4].to_f
-      }
-    end
+if selection == counter
+  #Creates Sale Complete
+  puts "===Sale Complete==="
+  puts ""
+  total = 0.0
+  transactions.each do |key, value|
+    puts "$#{products[key][:retail_price] * transactions[key]} - #{transactions[key]} #{products[key][:name]} "
+    total += products[key][:retail_price] * transactions[key]
   end
+  puts "\nTotal: #{currency_conv(total)}"
+
+  #Get Tendered
+  puts "\nWhat is the amount tendered?"
+  tendered = gets.chomp.to_f
+
+  #Creates Receipt
+  puts "===Thank You!==="
+  puts "The total change due is #{currency_conv(tendered-total)}\n\n "
+  puts Time.now.strftime("%m/%d/%Y %I:%M%p")
+  puts "================"
+elsif selection == counter + 1
+  #Creates Report
+  puts "What date would you like reports for? (MM/DD/YYYY)"
+  user_date = gets.chomp
+  until user_date.match(/(0[1-9]|1[012])[ \/.](0[1-9]|[12][0-9]|3[01])[ \/.](19|20)\d\d/)
+    puts "Invalid date, please format date"
+    user_date == gets.chomp
+  end
+  puts "On #{user_date} we sold:"
+  profit = 0.0
+  sales = 0.0
+  manager_report('report.csv', user_date).each do |key, value|
+    puts "SKU # #{key}, Name: #{value[:name]}, Quantity: #{value[:quantity]}, Revenue: #{currency_conv(value[:revenue])}, Profit: #{currency_conv(value[:profit])}"
+    profit += value[:profit]
+    sales += value[:revenue]
+  end
+  puts "Total Sales: #{currency_conv(sales)}"
+  puts "Total Profit: #{currency_conv(profit)}"
 end
 
-profit = 0.0
-sales = 0.0
-report_hash.each do |key, value|
-  puts "SKU # #{key}, Name: #{value[:name]}, Quantity: #{value[:quantity]}, Revenue: #{currency_conv(value[:revenue])}, Profit: #{currency_conv(value[:profit])}"
-  profit += value[:profit]
-  sales += value[:revenue]
-end
 
-puts "Total Sales: #{currency_conv(sales)}"
-puts "Total Profit: #{currency_conv(profit)}"
 
 
 
